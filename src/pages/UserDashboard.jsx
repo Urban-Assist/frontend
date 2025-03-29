1074583deabfeb66d87c48c819ac0a3f2123c696
Vaibhavkumar Patel <vaibhavpatel162002@gmail.com>
2025-03-29 14:11:40 -0300

    Add provider-specific service cards and update UserDashboard to reflect user role

--- src/pages/UserDashboard.jsx
+++ src/pages/UserDashboard.jsx
@@ -1,18 +1,25 @@
-
 import ServiceCards from '../components/ServicesCards';
+import { useEffect, useState } from 'react';
 
 function UserDashboard() {
+  const [userRole, setUserRole] = useState('');
+  
+  useEffect(() => {
+    const role = localStorage.getItem('role');
+    if (role) {
+      setUserRole(role);
+    }
+  }, []);
 
   return (
-
-      <div className="flex flex-col items-center p-4 sm:p-5 lg:p-12 min-h-screen mt-10">
-      <div class="flex items-start">
-
-       
-        <ServiceCards title="Our Premium Services"/>
-
+    <div className="flex flex-col items-center p-4 sm:p-5 lg:p-12 min-h-screen mt-10">
+      <div className="flex items-start">
+        <ServiceCards 
+          title={userRole === 'provider' ? 'Services You can Provide' : 'Our Premium Services'}
+        />
       </div>
     </div>
   );
 }
+
 export default UserDashboard;
