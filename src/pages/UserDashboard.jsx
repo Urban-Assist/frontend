d35f973c0af3cae1a0dee9300fef3fed7fa9f2a5
Vaibhavkumar Patel <vaibhavpatel162002@gmail.com>
2025-03-29 14:13:46 -0300

    Refactor UserDashboard layout for improved structure and spacing

--- src/pages/UserDashboard.jsx
+++ src/pages/UserDashboard.jsx
@@ -12,12 +12,11 @@ function UserDashboard() {
   }, []);
 
   return (
-    <div className="flex flex-col items-center p-4 sm:p-5 lg:p-12 min-h-screen mt-10">
-      <div className="flex items-start">
-        <ServiceCards 
-          title={userRole === 'provider' ? 'Services You can Provide' : 'Our Premium Services'}
-        />
-      </div>
+    <div className="min-h-screen">
+      <div className='mt-10'/>
+      <ServiceCards 
+        title={userRole === 'provider' ? 'Services You can Provide' : 'Our Premium Services'}
+      />
     </div>
   );
 }
